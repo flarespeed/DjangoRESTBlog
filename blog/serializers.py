7980aafcd51e3e90b1c6e3a27a8833e3365772d7
flarespeed <hflare@gmail.com>
2021-07-12 13:40:27 -0700

    made the user object show only username, not email

--- blog/serializers.py
+++ blog/serializers.py
@@ -5,7 +5,7 @@ from rest_framework import serializers
 class UserSerializer(serializers.HyperlinkedModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email']
+        fields = ['url', 'username']
 
 class NonRecursiveCommentSerializer(serializers.HyperlinkedModelSerializer):
     user = serializers.ReadOnlyField(source='user.username')
